114764644c134a8e9a9b1b0d4d5b229f620d632f
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-10 20:09:06 +0400

    for 15 create logger

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
+const { login, createUser, signOut } = require('./controllers/users');
 const error = require('./middlewares/error');
 const auth = require('./middlewares/auth');
 
@@ -17,12 +19,31 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
+app.use(cors({
+  origin: [
+    'https://mesto-krasivoe.nomoredomains.club',
+    'https://api.mesto-krasivoe.nomoredomains.club',
+    'http://mesto-krasivoe.nomoredomains.club',
+    'http://api.mesto-krasivoe.nomoredomains.club',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  credentials: true,
+  optionsSuccessStatus: 200,
+}));
 
 // Подлключение к БД mestodb
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -41,6 +62,8 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
+app.delete('/signout', signOut);
+
 // авторизация
 app.use(auth);
 
@@ -51,6 +74,7 @@ app.use('*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не существует');
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(error);
 
